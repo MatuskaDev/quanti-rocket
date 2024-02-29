474c084d40f765ca3e252deae082d11d6dee2e48
Lukáš Matuška <matuslu2@fit.cvut.cz>
2024-02-29 16:22:09 +0100

    Add RocketDetailView

---
+++ Rocket/Rocket/PresentationLayer/RocketDetail/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RocketDetail",
+    platforms: [.iOS(.v17)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "RocketDetail",
+            targets: ["RocketDetail"]),
+    ],
+    dependencies: [
+        .package(name: "SharedDomain", path: "../../DomainLayer/SharedDomain"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "RocketDetail",
+            dependencies: [
+                .product(name: "SharedDomain", package: "SharedDomain"),
+                .product(name: "SharedDomainMocks", package: "SharedDomain"),
+            ]
+        )
+    ]
+)
